970f3d60b4e323d5947670c20c7f40b4b48c66cd
Hugo Piauilino <hugos94@gmail.com>
2015-05-05 16:47:04 -0300

    Adicionado o centro da ufpi com a camera movendo-se para o marcador

--- app/src/main/java/com/ufpimaps/MapaNormalFragment.java
+++ app/src/main/java/com/ufpimaps/MapaNormalFragment.java
@@ -5,19 +5,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by HugoPiauilino on 30/04/15.
  */
-public class MapaNormalFragment extends android.support.v4.app.Fragment {
+public class MapaNormalFragment extends android.support.v4.app.Fragment implements OnMapReadyCallback {
 
     MapView mapView;
 
-    GoogleMap googleMap;
+    private GoogleMap googleMap;
+
+    private static final LatLng ufpiLocation = new LatLng(-5.057772,-42.797009);
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -28,7 +36,11 @@ public class MapaNormalFragment extends android.support.v4.app.Fragment {
 
         mapView.onCreate(savedInstanceState);
 
-        if(mapView!=null) {
+        mapView.getMapAsync(this);
+
+        MapsInitializer.initialize(getActivity());
+
+        if(mapView != null) {
             googleMap = mapView.getMap();
 
             googleMap.getUiSettings().setMyLocationButtonEnabled(true);
@@ -39,7 +51,13 @@ public class MapaNormalFragment extends android.support.v4.app.Fragment {
 
             googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
-            //googleMap.moveCamera(new CameraUpdateFactory(new LatLng(-5.057772, -42.797009), 14));
+            // Construct a CameraPosition focusing on Mountain View and animate the camera to that position.
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(ufpiLocation)      // Sets the center of the map to UFPI
+                    .zoom(15)                   // Sets the zoom
+                    .tilt(30)                   // Sets the tilt of the camera to 30 degrees
+                    .build();                   // Creates a CameraPosition from the builder
+            googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
         }
 
@@ -67,5 +85,12 @@ public class MapaNormalFragment extends android.support.v4.app.Fragment {
         super.onLowMemory();
 
         mapView.onLowMemory();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions()
+                .position(ufpiLocation)
+                .title("Universidade Federal do Piauí (UFPI)"));
     }
 }
