c034c4a437dfba7de9784720e5ad91dd07e16aa3
Hugo Piauilino <hugos94@gmail.com>
2015-05-28 22:15:47 -0300

    Funcionalidade Auto Completar da tela Tracar Rotas implementado.
    Obs.: Ainda está buscando por um Array genérico. Implementar a busca no banco de dados

--- app/src/main/java/com/ufpimaps/views/TraceRouteFragment.java
+++ app/src/main/java/com/ufpimaps/views/TraceRouteFragment.java
@@ -1,15 +1,15 @@
 package com.ufpimaps.views;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 
 import com.ufpimaps.R;
-
 /**
  * Created by HugoPiauilino on 14/05/15.
  */
@@ -20,10 +20,28 @@ public class TraceRouteFragment extends android.support.v4.app.Fragment {
      */
     private Button traceRouteButton;
 
+    private AutoCompleteTextView originEditText;
+    private AutoCompleteTextView destinationEditText;
+
+    private String[] search;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View traceRouteView = inflater.inflate(R.layout.fragment_trace_route, container, false);
+
+        originEditText = (AutoCompleteTextView) traceRouteView.findViewById(R.id.originEditText);
+        destinationEditText = (AutoCompleteTextView) traceRouteView.findViewById(R.id.destinationEditText);
+
+        search = getResources().getStringArray(R.array.countries_array);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, search);
+
+        originEditText.setAdapter(adapter);
+        destinationEditText.setAdapter(adapter);
+
+
         traceRouteButton = (Button) traceRouteView.findViewById(R.id.trace_route_button);
         traceRouteButton.setOnClickListener(new View.OnClickListener() {
             @Override
