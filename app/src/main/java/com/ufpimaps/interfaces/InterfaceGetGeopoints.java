33a1919d2ff3de80fead0be65d0a3e187c80427c
zenote <joselinsneto182@hotmail.com>
2015-05-29 15:12:04 -0300

    Interface que permite os dados gerados dentro da AsyncTask serem repassados para outra classe

---
+++ app/src/main/java/com/ufpimaps/interfaces/InterfaceGetGeopoints.java
@@ -0,0 +1,8 @@
+package com.ufpimaps.interfaces;
+
+/**
+ * Created by zenote on 29/05/2015.
+ */
+public interface InterfaceGetGeopoints {
+    public void devolverGeopoints(String[] geopoints);
+}
