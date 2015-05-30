df35412a1d602e25ffc480da33f2ecbdcf9c6da5
zenote <joselinsneto182@hotmail.com>
2015-05-29 23:44:55 -0300

    A classe ApplicationObject contém os atributos ou objetos que serão utilizados em vários locais da aplicação, como, por exemplo, o mapa

---
+++ app/src/main/java/com/ufpimaps/models/ApplicationObject.java
@@ -0,0 +1,13 @@
+package com.ufpimaps.models;
+
+import android.app.Application;
+
+import com.ufpimaps.views.MapFragment;
+
+/**
+ * Created by zenote on 29/05/2015.
+ */
+public class ApplicationObject extends Application{
+
+    public MapFragment mapa = new MapFragment();
+}
