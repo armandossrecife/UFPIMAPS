714da9a49149d334df1f38eb53c065d8929aad9b
zenote <joselinsneto182@hotmail.com>
2015-05-29 23:43:20 -0300

    Criada InterfaceGetListOfGeoPoints para devolver a lista de Geopoints da AsyncTaskTraceRoute para o fragmento MapFragment

---
+++ app/src/main/java/com/ufpimaps/interfaces/InterfaceGetListOfGeoPoints.java
@@ -0,0 +1,12 @@
+package com.ufpimaps.interfaces;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
+
+/**
+ * Created by zenote on 29/05/2015.
+ */
+public interface InterfaceGetListOfGeoPoints {
+    public void devolveListaDeGeoPoints(List<LatLng> lista);
+}
